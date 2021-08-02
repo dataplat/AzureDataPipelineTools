c11e8062e082a1c37a8e044b33d1a70dac519046
Niall Langley <niall.langley@gmail.com>
2021-08-02 16:26:24 +0100

    Integration test base class can retrive secrets from key vault using secret names in the run settings file generated by the ARM template

--- DataPipelineTools.Functions.Tests/IntegrationTestBase.cs
+++ DataPipelineTools.Functions.Tests/IntegrationTestBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using DataPipelineTools.Tests.Common;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
 
 namespace DataPipelineTools.Functions.Tests
 {
@@ -10,6 +12,17 @@ namespace DataPipelineTools.Functions.Tests
     /// </summary>
     public abstract class IntegrationTestBase : TestBase
     {
+        public IntegrationTestBase()
+        {
+            if (TestContext.Parameters.Count == 0)
+                throw new ArgumentException("No setting file is configured for the integration tests.");
+        }
+
+
+
+
+
+
         protected bool IsRunningOnCIServer
         {
             get
@@ -30,6 +43,27 @@ namespace DataPipelineTools.Functions.Tests
         {
             throw new NotImplementedException();
         }
+
+        protected string GetKeyVaultSecretValue(string secretName)
+        {
+            /* For some reason the DefaultAzureCredential (SharedTokenCacheCredential / VisualStudioCredential) returns a 403 trying to access the key vault, even when access policies are configured correctly
+             * We either use one of the following to authenticate:
+             *  - var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions{ ExcludeSharedTokenCacheCredential = true, ExcludeVisualStudioCredential = true});
+             *  - var credential = new ChainedTokenCredential(new ManagedIdentityCredential(), new AzureCliCredential());
+             *
+             * See here for more info: https://docs.microsoft.com/en-us/answers/questions/74848/access-denied-to-first-party-service.html
+             */
+            var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ExcludeSharedTokenCacheCredential = true, ExcludeVisualStudioCredential = true });
+
+
+            var keyVaultUri = $"https://{TestContext.Parameters["KeyVaultName"]}.vault.azure.net";
+            var client = new SecretClient(new Uri(keyVaultUri), credential);
+            var result = client.GetSecretAsync(secretName).Result;
+
+            return result?.Value?.Value;
+        }
+
+        
 
     }
 }
