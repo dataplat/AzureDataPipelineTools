26554e049d343539140f8c3e42453c397c3f76f1
Niall Langley <niall.langley@gmail.com>
2021-08-09 13:16:37 +0100

    Added a JSON array parameter to the ARM template for additional principals to be granted access to the lab resources

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,18 +5,9 @@ echo "Connection info:"
 az account show | jq '. | {tenantId: .tenantId, subscriptionName: .name, userName: .user.name, userType: .user.type}'
 echo
 
-echo "Configuring variables for secrets:"
 echo "========================================================================================================================================================================================================"
-echo
 
-# ls -a
-
-
-# # Can we run a shell script to tidy this up?
-# . ./.github/workflows/deploy_dev_test_lab.sh -?
-
-# echo "========================================================================================================================================================================================================"
-
+echo "Configuring variables for secrets:"
 
 ARTIFACT_SOURCE_NAME=$(az lab artifact-source list --resource-group $RESOURCE_GROUP \
                                                     --lab-name $LAB_NAME \
@@ -39,16 +30,16 @@ echo "Service Principal Object Id:     $SERVICEPRINCIPALID"
 
 # Build a JSON snippet with the client/app id, object id and client secret for the devops SPN. This is used by the ARM template to grant permissions on resources so that the devops SPN
 # can deploy code into them. The ARM template generates the required .runsettings file for the integration tests as an output, which reuses the devops SPN to access resources to test.
-SERVICEPRINCIPALINFO=$( echo $SERVICEPRINCIPALCREDENTIALS | jq '{ tenantId, clientId, clientSecret, $clientObjectId }' --arg 'clientObjectId' $SERVICEPRINCIPALID -c )
-
-echo "Service Principal Info:          $SERVICEPRINCIPALINFO"
+SERVICE_PRINCIPAL_INFO=$( echo $SERVICE_PRINCIPAL_CREDENTIALS | jq '{ tenantId, clientId, clientSecret, $clientObjectId }' --arg 'clientObjectId' $SERVICEPRINCIPALID -c )
+echo "Service Principal Info:          $SERVICE_PRINCIPAL_INFO"
 
 echo "Building parameters file for ARM deployment..."
 PARAMETERS_FILE="$(pwd)/azuredeploy.parameters.json"
 echo $'[ { "name":"branch", "value":"'$BRANCH_NAME'" },' \
     '  { "name":"commit", "value":"'$GITHUB_SHA'" },' \
     '  { "name":"location", "value":"UK South" },' \
-    '  { "name":"devopsServicePrincipalCredentials", "value":' $SERVICEPRINCIPALINFO ' }' \
+    '  { "name":"devopsServicePrincipalCredentials", "value":' $SERVICE_PRINCIPAL_INFO ' },' \
+    '  { "name":"additionalPrincipals", "value":' "${ADDITIONAL_PRINCIPALS:=[]}" ' }' \
     ']' \
 | jq '.' > "$PARAMETERS_FILE"
 #cat $PARAMETERS_FILE
@@ -84,20 +75,22 @@ echo "::error Error provisioning lab environment"
 exit 1
 fi
 
-echo "========================================================================================================================================================================================================"
 DEPLOYMENTOUTPUT=$(az deployment group list --resource-group $ENVIRONMENT_INSTANCE_RESOURCE_GROUP_NAME --query '[0].properties.outputs')
 
-# DEBUG: Use this to get the full deployment output JSON. If the ARM template outputs a full reference to a resource, we can find the bits we need easily.
-# echo "::set-output name=DEPLOYMENTOUTPUT::$DEPLOYMENTOUTPUT"
+echo "Setting Job Outputs"
+echo "========================================================================================================================================================================================================"
 
-echo "Deployment Outputs"
-#echo "::set-output name=STORAGE_ACCOUNTCONNECTION_STRING::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.storageAccountConnectionString.value')"
+
+# These don't show in the output, but we can view then in a yaml step as below
+#   echo "ENVIRONMENT_INSTANCE_NAME:                ${{ steps.create-devtest-labs-environment.outputs.ENVIRONMENT_INSTANCE_NAME }}"
+
+# DEBUG: Use this to get the full deployment output JSON. If the ARM template outputs a full reference to a resource, we can find the bits we need easily.
+#echo "::set-output name=DEPLOYMENTOUTPUT::$DEPLOYMENTOUTPUT"
+
 echo "::set-output name=STORAGE_ACCOUNT_NAME::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.storageAccountName.value')"
 echo "::set-output name=STORAGE_CONTAINER_NAME::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.storageContainerName.value')"
 echo "::set-output name=FUNCTIONS_APP_NAME::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.functionsAppName.value')"
 echo "::set-output name=FUNCTIONS_APP_URI::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.functionsAppUri.value')"
 echo "::set-output name=KEY_VAULT_NAME::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.keyVaultName.value')"
-#echo "::set-output name=FUNCTIONS_APP_KEY::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.functionsAppKey.value')"
 echo "::set-output name=RUN_SETTINGS::$(echo $DEPLOYMENTOUTPUT | jq --raw-output '.runSettings.value')"
 
-echo "========================================================================================================================================================================================================"
