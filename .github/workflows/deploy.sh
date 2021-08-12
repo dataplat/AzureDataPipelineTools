067178824b1a8ed403dfa7a51a7797a4e5b0084f
Niall Langley <niall.langley@gmail.com>
2021-08-12 16:48:26 +0100

    Improving lab resource naming to allow re-runs and manual runs for dev environments

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,6 +4,7 @@ echo
 echo "Connection info:"
 az account show | jq '. | {tenantId: .tenantId, subscriptionName: .name, userName: .user.name, userType: .user.type}'
 echo
+RUNTIMESTAMP=$(date +"%Y%m%d%H%M")
 
 echo "========================================================================================================================================================================================================"
 
@@ -20,6 +21,46 @@ echo "Artifact Source Name: $ARTIFACT_SOURCE_NAME"
 BRANCH_NAME=${GITHUB_REF#*refs/heads/}
 echo "Branch Name: $BRANCH_NAME"
 
+# Build some id's for the names of resources
+if [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]; then
+    ACTOR_NAME='DEV_'"$(echo $GITHUB_ACTOR | sed "s/[^[:alpha:][:digit:]]//g")"
+    ACTOR_SHORT=$(echo ${GITHUB_ACTOR} | sed "s/[^[:alpha:][:digit:]]//g" | cut -c -9)
+    ENVIRONMENT=DEV
+else
+    ACTOR_NAME=GITHUB_CI_BUILD
+    ACTOR_SHORT=CI
+    ENVIRONMENT=CICD
+fi
+
+
+if [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]; then
+    REASON_TAG="Dev Environment for $GITHUB_ACTOR"
+elif [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
+    REASON_TAG="CI - ${GITHUB_SHA:0:7} pushed by $GITHUB_ACTOR for "
+else
+    REASON_TAG="CI - ${GITHUB_SHA:0:7} pushed by $GITHUB_ACTOR"
+fi
+
+
+echo "GitHub Actor: ${GITHUB_ACTOR}"
+echo "Actor Short: ${ACTOR_SHORT}"
+echo "Actor Name: ${ACTOR_NAME}"
+echo "GitHub Run Number: ${GITHUB_RUN_NUMBER}"
+echo "GitHub Run Id: ${GITHUB_RUN_ID}"
+echo "GitHub Event Name: ${GITHUB_EVENT_NAME}"
+echo "GitHub Workflow URL: ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+echo "GitHub PR: ${GITHUB_PR_NUMBER}: $GITHUB_PR_TITLE"
+
+
+# The name of the lab. Allows sorting by owner, timestamp, while showing the branch and commit it was built from
+#ENVIRONMENT_INSTANCE_NAME='CI_Build___'"${BRANCH_NAME////__}"'___'"${GITHUB_SHA:0:8}"''
+ENVIRONMENT_INSTANCE_NAME="${ACTOR_NAME}"'__'"${RUNTIMESTAMP}"'__'"${BRANCH_NAME////__}"'___'"${GITHUB_SHA:0:7}"''
+echo "Environment Instance Name: $ENVIRONMENT_INSTANCE_NAME"
+
+# Used for resource names, eg adlsnlangley1602c96a22d or adlsci09498d47f45. This should give enough uiniqueness to allow parallel environments, while showing the build reason and owner
+RESOURCE_NAME_SUFFIX="_${ACTOR_SHORT}_${RUNTIMESTAMP:8:12}_${GITHUB_SHA:0:7}"
+echo "Resource Name Suffix: $RESOURCE_NAME_SUFFIX"
+
 # We need the object id of the Enterprise Application created from the App Registration in order to set permissions in the ARM template. This is **not** the same as the app/client id
 echo "Retriving service principal id for the logged in user..."
 SERVICEPRINCIPALAPPID=$(az account show | jq --raw-output '.user.name')
@@ -35,8 +76,14 @@ echo "Service Principal Info:          $SERVICE_PRINCIPAL_INFO"
 
 echo "Building parameters file for ARM deployment..."
 PARAMETERS_FILE="$(pwd)/azuredeploy.parameters.json"
-echo $'[ { "name":"branch", "value":"'$BRANCH_NAME'" },' \
-    '  { "name":"commit", "value":"'$GITHUB_SHA'" },' \
+echo $'[' \
+    '  { "name":"branch", "value":"'$BRANCH_NAME'" },' \
+    '  { "name":"environment", "value":"'$ENVIRONMENT'" },' \
+    '  { "name":"environmentUser", "value":"'$ACTOR_NAME'" },' \
+    '  { "name":"gitSha", "value":"'$GITHUB_SHA'" },' \
+    '  { "name":"gitShaShort", "value":"'${GITHUB_SHA:0:7}'" },' \
+    '  { "name":"githubPullRequest", "value":"PR: '${GITHUB_PR_NUMBER}': '$GITHUB_PR_TITLE'" },' \
+    '  { "name":"resourceNameSuffix", "value":"'$RESOURCE_NAME_SUFFIX'" },' \
     '  { "name":"location", "value":"UK South" },' \
     '  { "name":"devopsServicePrincipalCredentials", "value":' $SERVICE_PRINCIPAL_INFO ' },' \
     '  { "name":"additionalPrincipals", "value":' "${ADDITIONAL_PRINCIPALS:=[]}" ' }' \
@@ -44,8 +91,6 @@ echo $'[ { "name":"branch", "value":"'$BRANCH_NAME'" },' \
 | jq '.' > "$PARAMETERS_FILE"
 #cat $PARAMETERS_FILE
 
-ENVIRONMENT_INSTANCE_NAME='CI_Build___'"${BRANCH_NAME////__}"'___'"${GITHUB_SHA:0:8}"''
-echo "Environment Instance Name: $ENVIRONMENT_INSTANCE_NAME"
 
 echo "::set-output name=ENVIRONMENT_INSTANCE_NAME::$ENVIRONMENT_INSTANCE_NAME"
 
@@ -83,7 +128,6 @@ echo "==========================================================================
 
 
 # These don't show in the output, but we can view then in a yaml step as below
-#   echo "ENVIRONMENT_INSTANCE_NAME:                ${{ steps.create-devtest-labs-environment.outputs.ENVIRONMENT_INSTANCE_NAME }}"
 
 # DEBUG: Use this to get the full deployment output JSON. If the ARM template outputs a full reference to a resource, we can find the bits we need easily.
 #echo "::set-output name=DEPLOYMENTOUTPUT::$DEPLOYMENTOUTPUT"
